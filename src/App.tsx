9840e5198b053d1b72423e43beb784354fa3644f
Gabriel Jorge da Cruz <gabrielbielcruz1.007@gmail.com>
2026-01-15 10:20:09 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import './App.css';
+import Distritos from "./distritos";
+
+function App() {
+
+
+  return (
+      <MapContainer
+        style={{ height: '100vh', width: '100%' }}
+        center={[-23.5442026, -46.6322936]}
+        zoom={11}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Distritos />
+      </MapContainer>
+  )
+}
+
+export default App
